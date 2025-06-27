fa9f92ec7332ad4befb83898ba9879f7d668102c
Victor Rodrigues <victor.rodrigues.carvalho.santos@gmail.com>
2025-06-27 19:24:59 +0000

    feat: :label: Add new types Size and TerminalCommand

--- lib/main.ts
+++ lib/main.ts
@@ -1,3 +1,10 @@
 // lib/main.ts
 import "./main.css";
-export { Container } from "./components/Container";
+export * from "./components/Container";
+export * from "./components/Terminal";
+export * from "./components/TerminalContext";
+export * from "./components/Input";
+export * from "./components/RightArrow";
+export * from "./hooks/useTerminal";
+export type * from "./types/Size";
+export type * from "./types/TerminalCommand";
