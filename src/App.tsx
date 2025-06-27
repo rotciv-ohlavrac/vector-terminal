deec3534fa872d928d511d534ab00b0fb4e52de4
Victor Rodrigues <victor.rodrigues.carvalho.santos@gmail.com>
2025-06-27 19:25:21 +0000

    docs: :memo: Implement terminal on showcase page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import { Container } from "../lib/main";
+import {
+  Container,
+  Input,
+  Terminal,
+  TerminalContextProvider,
+} from "../lib/main";
+import type { TerminalCommand } from "../lib/types/TerminalCommand";
 
 import "./App.css";
 
 function App() {
+  const commands: Array<TerminalCommand> = [
+    {
+      input: "whois",
+      arguments: [],
+      execute: () => "Victor Rodrigues (Gelado)",
+    },
+  ];
   return (
     <>
-      <div className="h-full flex flex-col">
-        <h1>Hello World!</h1>
-        <Container>Container</Container>
+      <div className="h-full flex flex-col justify-center items-center">
+        <Container size="full">
+          <TerminalContextProvider commands={commands}>
+            <Terminal>
+              <Input />
+            </Terminal>
+          </TerminalContextProvider>
+        </Container>
       </div>
     </>
   );
