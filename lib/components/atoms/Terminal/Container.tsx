20567769b29891913eb111e2c752be3340d4ed64
Victor Rodrigues <victor.rodrigues.carvalho.santos@gmail.com>
2025-07-17 12:51:23 +0000

    feat: add TerminalContainer component for customizable terminal layout

---
+++ lib/components/atoms/Terminal/Container.tsx
@@ -0,0 +1,32 @@
+import { cva } from "class-variance-authority";
+import type { DetailedHTMLProps, HTMLAttributes } from "react";
+import type { Size } from "@/types";
+
+interface TerminalContainerProps
+  extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  size?: Size;
+}
+
+const containerStyles = cva(["bg-black", "p-4"], {
+  variants: {
+    size: {
+      sm: ["h-72", "w-72"],
+      md: ["h-80", "w-80"],
+      lg: ["h-96", "w-96"],
+      full: ["h-screen", "w-full"],
+    },
+  },
+});
+
+function TerminalContainer({
+  className = "",
+  size = "full",
+  ...props
+}: TerminalContainerProps) {
+  return (
+    <div className={`${containerStyles({ size })} ${className}`} {...props} />
+  );
+}
+
+export { TerminalContainer as Container };
+export type { TerminalContainerProps };
